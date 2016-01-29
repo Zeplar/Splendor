e30e17921cb584758f51e0d4829d1b9a7a7bb9e6
ZEPLAR <Zeplar1@gmail.com>
2016-01-28 23:26:02 -0800

    Fixed a bug where predictions for Greedy's next move were based on Exact's perspective.

--- Splendor/Exact/ExactFit.cs
+++ Splendor/Exact/ExactFit.cs
@@ -1,18 +1,105 @@
-﻿Up
+﻿using System.Collections.Generic;
+using AForge.Genetic;
+using System.Diagnostics;
+using System;
+
+namespace Splendor.Exact
+{
+    public class ExactFit : IFitnessFunction
+    {
+        private ScoringMethods.Function scoringFunction;
+        private bool wonPreviousGeneration = false;
+        private ScoringMethods.Function greedy = ScoringMethods.Points.opponent();
+
+        public ExactFit(ScoringMethods.Function scoringFunction)
+        {
+            this.scoringFunction = scoringFunction;
+        }
+
+        public double Evaluate(IChromosome chromosome)
+        {
+            return score((ExactChromosome)chromosome);
+
+        }
+
+        public Move getExactMove(Board b)
+        {
+            List<Move> moves = Move.getAllLegalMoves(b);
+            if (moves.Count == 0)
+            {
+                return null;
+            }
+            return moves[GameController.random.Next(moves.Count)];
+        }
+       
+
+        /// <summary>
+        /// Generates the next boardstate or returns false if unable
+        /// </summary>
+        private Board generate(ExactChromosome max, Board b)
+        {
+            Debug.Assert(b.turn % 2 == 0);
+            int i = b.turn / 2;
+            if (max.moves.Count <= i)
+            {
+                max.moves.Add(null);
+            }
+            if (max.moves[i] == null || !max.moves[i].isLegal(b))
+            {
+                max.moves[i] = getExactMove(b);
+                if (max.moves[i] == null)
+                {
+                    return null;
+                }
+            }
+            max.boardState[i] = hash(b);
+            return b.generate(max.moves[i]);
+        }
+
+        private bool predictWin(Board current, Move pred)
+        {
+            if (current.gameOver)
+            {
+                if (current.winner == 0) { RecordHistory.record("!!! " + GameController.currentPlayer + " now thinks it's going to win! Pred. Greedy move " + pred); wonPreviousGeneration = true; }
+                else if (current.winner == 1) { RecordHistory.record("!!! " + GameController.currentPlayer + " now thinks it's going to lose!"); wonPreviousGeneration = false; }
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Evaluates a chromosome against Greedy
+        /// </summary>
+        private double score(ExactChromosome max)
+        {
+            Board current = Board.current;
+            Board next;
+            Move nextMove;
+            Move pred = null; //Predicted move next turn for Greedy
+            double score = 0;
+            int i = 0;
+            while (current.turn < max.length)
+            {
+                if (predictWin(current, pred)) break;
+                next = generate(max, current);
+                if (next == null) break;
                 score += scoringFunction.evaluate(next);
+                i++;
                 current = next;
+                if (predictWin(current, pred)) break;
                 //Do the "Generate-next-move" loop for greedy
-                nextMove = Greedy.getGreedyMove(current, ScoringMethods.Lead);
+                nextMove = Greedy.getGreedyMove(current, greedy);
+                if (i == 1) pred = nextMove;
                 if (nextMove != null)
                 {
                     current = current.generate(nextMove);
                 }
                 else
                 {
-                    return score;
+                    return score / i;
                 }
             }
-            return score;
+            return score / i;
         }
 
         private uint hash(Board b)
